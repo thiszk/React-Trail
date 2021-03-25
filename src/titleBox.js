88423d3e69a7f82183b06af4c877fe896e6615fb
Thiago <thiago.saziki@gmail.com>
2021-03-25 19:17:01 -0300

    Page Title and info (mock)

--- src/titleBox.js
+++ src/titleBox.js
@@ -2,10 +2,22 @@ import React from 'react';
 import Logo from './img/RaM_logo.png';
 
 function TitleLogo() {
-        return <img className="titleLogo" src={Logo} alt="Title Logo"/>
+    return <img className="titleLogo" src={Logo} alt="Title Logo"/>
 }
+
 function MainTitle() {
-        return <p className="title">Rick and Morty</p>
+    return <p className="mainTitle">Rick and Morty</p>
+}
+
+function NumberOfEpisodes() {
+    return <p className="numberOfEpisodes">No. of Episodes : 41 </p>    
+}
+function TitleDescription() {
+    return <p className="titleDescription">Rick and Morty is an American adult animated science fiction 
+    sitcom created by Justin Roiland and Dan Harmon for Cartoon Network's nighttime Adult Swim 
+    programming block. The series follows the misadventures of cynical mad scientist Rick Sanchez 
+    and his good-hearted but fretful grandson Morty Smith, who split their time between domestic 
+    life and interdimensional adventures.</p>
 }
 
 export function TitleBox() {
@@ -13,6 +25,8 @@ export function TitleBox() {
         <div className="titleBox">
             <TitleLogo/>
             <MainTitle/>
+            <NumberOfEpisodes/>
+            <TitleDescription/>
         </div>
     )
 }
