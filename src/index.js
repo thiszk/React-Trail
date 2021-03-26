d867b3cc02f2a1cf4db9209275970b7b29380dd9
Thiago <thiago.saziki@gmail.com>
2021-03-25 23:02:07 -0300

    Render EpisodeBox Component (EpisodeList + NavigationBar)

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { TitleBox } from './titleBox.js';
+import { EpisodeList } from './episodeList.js';
+import { EpisodeBox } from './episodeBox';
 
   ReactDOM.render(
     <TitleBox />,
-    document.getElementById('root')
+    document.getElementById('titleBox')
+  );
+  ReactDOM.render(
+      <EpisodeBox />,
+      document.getElementById('container')
   );
